9c5b780ea9b571ef3cb6a434a2434ff70862aba0
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2025-04-18 11:33:43 +0200

    Successfully exposing first functions via GET and POST

--- wrapper.py
+++ wrapper.py
@@ -8,6 +8,13 @@ TAG = "expose_as"
 
 class InterfaceFunction:
     file_to_read = None
+    generated_error_model = False
+    error_model_name = 'ErrorOutput'
+    mod_parameter = 'use_filesystem'
+    mod_msg = """Please not that the server filesystem is never used when calling these APIs 
+    (parameter use_filesystem is always set to False).
+    """
+    mod_code = f"{mod_parameter} = False # No server filesystem access via API"
 
     def __init__(self, node: ast.FunctionDef, method: str, file_to_read:str):
         self.node = node
@@ -16,8 +23,14 @@ class InterfaceFunction:
         self.method = method
         InterfaceFunction.file_to_read = file_to_read
     
-    def get_docstring(self):
-        ret = ast.get_docstring(self.node) or ""
+    def get_docstring(self, arg_names):
+        docstr = ast.get_docstring(self.node) or ""
+        if InterfaceFunction.mod_parameter in arg_names:
+            ret = f"""{docstr}
+{InterfaceFunction.mod_msg}
+"""
+        else:
+            ret = docstr
         return repr(ret)
 
     @staticmethod
@@ -37,31 +50,87 @@ class InterfaceFunction:
             return t_str
         return t_str
 
-    def generate_wrapper(self) -> str:
-        arg_names = [arg.arg for arg in self.args if arg.arg != 'self']
+    def _get_args(self):
+        nr_arg = len(self.args)
+        nr_defaults = len(self.node.args.defaults)
+        args = []
+        for i in range(nr_arg):
+            a = self.args[i]
+            idx = nr_arg-i-1
+            default = ast.unparse(self.node.args.defaults[idx]) if (idx >= 0 and idx < nr_defaults) else None
+            args.append((a.arg, self.resolve_type(a.annotation), default))
+        return args
+    
+    def generate_models(self):
+        """
+            Create input model for POST and output models for GET and POST
+            Also create a generic output model for errors
+        """
+        code = []
+        if not InterfaceFunction.generated_error_model:
+            code.append(f"class {InterfaceFunction.error_model_name}(BaseModel):")
+            code.append(f"    error: str")
+            InterfaceFunction.generated_error_model = True
         
-        if self.method == 'post':    
+        if self.method == "post":
+            args = self._get_args()
+            if len(args) > 0:
+                code.append(f"class {self.name.title()}Input(BaseModel):")
+                
+                for arg_name, arg_type, arg_default in args:
+                    # breakpoint()
+                    default = arg_default if arg_default else ""
+                    code.append(f"    {arg_name}: {arg_type}{' = ' if default else ''}{default}")
+        code.append("\n")
+        code.append(f"class {self.name.title()}Output(BaseModel):")
+        # breakpoint()
+        if self.method == "post":
+            if self.resolve_type(self.node.returns) != 'None':
+                # breakpoint()
+                raise Exception(f'{self.name}: only no return allowed for original functions exposed as post')
+            code.append(f"    result: {self.name.title()}Input\n")
+        else:
+            if self.resolve_type(self.node.returns) == 'None':
+                # breakpoint()
+                raise Exception(f'{self.name}: must return something if exposed as get')
+            returns = self.resolve_type(self.node.returns)
+            code.append(f"    result: {returns}\n")
+
+        return "\n".join(code)
+        
+    def generate_wrapper(self) -> str:
+        
+        call = (f"@app.{self.method.lower()}('/{self.name}', response_model={self.name.title()}Output|{InterfaceFunction.error_model_name})")
+        arg_names = [arg.arg for arg in self.args if arg.arg != 'self']
+        if self.method == 'post':
             # args_unpack = ", ".join(f"{arg}=payload.{arg}" for arg in arg_names)
-            args_copy = "\n    ".join(f"{arg} = copy.deepcopy(payload.{arg})" for arg in arg_names)
+            args_copy = "\n    ".join(f"{arg} = copy.deepcopy(payload.{arg})" for arg in arg_names if arg != InterfaceFunction.mod_parameter)
             return_keys = ", ".join(f'\"{arg}\": {arg}' for arg in arg_names)
+            fn_args = f'payload: {self.name.title()}Input' if len(arg_names)>0 else ''
             code = f"""
-@app.{self.method.lower()}("/{self.name}")
-def route_{self.name}(payload: {self.name.title()}Input):
-    ''{self.get_docstring()}''
+{call}
+def {self.name}_endpoint({fn_args}):
+    ''{self.get_docstring(arg_names)}''
+    
     import copy
     {args_copy}
-    result = None
+    {InterfaceFunction.mod_code if InterfaceFunction.mod_parameter in arg_names else ''}
     try:
-        result = {self.file_to_read}.{self.name}({', '.join(arg_names)})
+        {self.file_to_read}.{self.name}({', '.join(arg_names)})
     except Exception as e:
         return {{"error": str(e)}}
-    return {{"result": result if result is not None else {{ {return_keys} }} }}
+    return {{"result": {{ {return_keys} }} }}
 """
         else:
+            params = ", ".join([
+                f"{arg}: {typ}{' = ' if default else ''}{default if default else ''}"
+                for arg, typ, default in self._get_args()
+                if arg != "self"
+            ])
             code = f"""
-@app.{self.method.lower()}("/{self.name}")
-def route_{self.name}(payload: {self.name.title()}Input):
-    ''{self.get_docstring()}''
+{call}
+def {self.name}_endpoint({params}):
+    ''{self.get_docstring(arg_names)}''
     result = None
     try:
         result = {self.file_to_read}.{self.name}({', '.join(arg_names)})
@@ -90,17 +159,6 @@ def extract_interface_functions(file_to_read):
                 functions.append(InterfaceFunction(node, method, file_to_read.stem))
     return functions
 
-def generate_input_models(funcs: List[InterfaceFunction]) -> str:
-    models = []
-    for func in funcs:
-        fields = [f"    {arg.arg}: Any" for arg in func.args if arg.arg != 'self']
-        model = f"""
-class {func.name.title()}Input(BaseModel):
-{chr(10).join(fields)}
-"""
-        models.append(textwrap.dedent(model))
-    return "\n".join(models)
-
 
 def build_fastapi_code(functions):
     code = [
@@ -112,12 +170,12 @@ def build_fastapi_code(functions):
         "app = FastAPI()",
         "",
     ]
-    code.append(generate_input_models(functions))
 
     for func in functions:
+        code.append(func.generate_models())
         wrapper = func.generate_wrapper()
         code.append(textwrap.dedent(wrapper))
-
+    # breakpoint()
     return "\n".join(code)
     
 
