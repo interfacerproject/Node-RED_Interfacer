6145fcae8989e5a511e5750b3c550bbf224fe0cd
vortex <adamb@free2air.net>
2025-08-19 11:23:23 +0200

    adds default port setting

--- startNodeRed.sh
+++ startNodeRed.sh
@@ -25,5 +25,6 @@ then
     docker run -it -p 1880:1880 -v ${PWD}:/data --name ${cont_name} nodered/node-red
 else
     # https://nodered.org/docs/getting-started/local
-    ./node_modules/.bin/node-red --settings ./nodered_conf/settings.js
+    [ ! -v NODERED_PORT ] && NODERED_PORT=1880
+    ./node_modules/.bin/node-red --port ${NODERED_PORT} --settings ./nodered_conf/settings.js
 fi
